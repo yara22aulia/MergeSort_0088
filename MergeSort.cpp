2f5c892130be80605ec63b2cfafb0677bb5970a7
yara22aulia <yara22aulia@gmail.com>
2026-04-02 18:18:56 +0700

    implementasi logika utama penggabungan (step 4.d

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -97,7 +97,8 @@ void output(
 )
 {
     cout << "\nData setelah diurutkan (Merge Sort): ";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++
+    )
     {
         cout << arr[i] << " ";
     }
